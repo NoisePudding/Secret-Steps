5821db7655ae61adcb64576120c0769328249bfc
Noise Pudding <noisepudding@gmail.com>
2024-11-30 13:04:28 -0300

    first build

--- scenes/mouse_light.gd
+++ scenes/mouse_light.gd
@@ -11,5 +11,5 @@ func _process(_delta: float) -> void:
 	if is_locked == false:
 		global_position = get_global_mouse_position()
 	
-	if Input.is_action_just_pressed("lock"):
-		is_locked = not is_locked
+	#if Input.is_action_just_pressed("lock"):
+		#is_locked = not is_locked
